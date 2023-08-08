6036f3f19864aaa9840a270e264132a13bf1074d
ldw <2632711107@qq.com>
2023-08-08 17:30:30 +0800

    全局异常处理

--- parent/yoa-server/src/main/java/com/server/config/GlobalException.java
+++ parent/yoa-server/src/main/java/com/server/config/GlobalException.java
@@ -1,7 +1,23 @@
 package com.server.config;
 
+import com.server.vo.common.RespBean;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+
 /**
- *
+ *全局异常处理
  */
+@RestControllerAdvice //表示是控制器的增强类
 public class GlobalException {
+    //捕捉异常，sql
+    @ExceptionHandler(SQLException.class)
+    public RespBean mySqlException(SQLException e){
+        if (e instanceof SQLIntegrityConstraintViolationException){
+            return RespBean.error("该数据有关联数据，操作失败！");
+        }
+        return RespBean.error("数据库异常，操作失败！");
+    }
 }
